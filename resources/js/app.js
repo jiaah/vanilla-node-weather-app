2bc51f2ac6783131b1c9e2ece9d36b7e8284bf32
Jin827 <hyojin.lee827@gmail.com>
2018-06-05 17:39:09 +0900

    change image

--- resources/js/app.js
+++ resources/js/app.js
@@ -165,7 +165,7 @@
         const backgroundImg = document.getElementById('section-weather');
 
         if (temperature > 20) {
-            return backgroundImg.style.backgroundImage = "url('resources/img/clear-sky.png')";
+            return backgroundImg.style.backgroundImage = "url('resources/img/clear-sky.jpg')";
         }
         if (temperature > 0) {
             return backgroundImg.style.backgroundImage = "url('resources/img/cloud.png')";
